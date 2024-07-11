501306e95e6ed05100e35d4822c2d1812afd513a
yllhyseni <47318986+yllhyseni@users.noreply.github.com>
2024-07-11 11:51:05 +0200

    Delete grade-checker-main/grupi veror yll directory

--- grade-checker-main/grupi veror yll/index3.py
+++
@@ -1,5 +0,0 @@
-numri = int(input("shkruaj nje numer:"))
-if (numri % 2 == 0):
-    print(f"numri: {numri} eshte qift")
-else:
-    print(f"numri: {numri} eshte tek ")    
--- grade-checker-main/grupi veror yll/simple_login.py
+++
@@ -1,8 +0,0 @@
-while True:
-    username = input("sheno username:")
-    if(username != "yllhyseni"):
-        continue
-    password = input("sheno passwordin:")
-    if(password == "jcoders"):
-        break
-print("emri dhe passwordi jan ne rregull")
